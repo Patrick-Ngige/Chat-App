6a34c61770b180930cd7f6ec6478bf5249319c75
Patrick-Ngige <wakemanjajr@gmail.com>
2023-09-21 15:54:32 +0300

    creating the register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -4,11 +4,16 @@ const Register = () => {
   return (
     <div className='formContainer'>
       <div className="formWrapper">
+        <span className='logo'>Chat App</span>
+        <span className='title'>Register</span>
         <form>
             <input type='text' placeholder='display name'/>
             <input type='email' placeholder='email'/>
             <input type='password' placeholder='password'/>
+            <input type="file" />
+            <button>Sign Up</button>
         </form>
+        <p>You do have an account? Login here</p>
       </div>
     </div>
   )
