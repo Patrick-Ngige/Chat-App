cede835850cb53a0114b9153b5b6242321aa38a4
Patrick-Ngige <wakemanjajr@gmail.com>
2023-09-25 23:03:47 +0300

    creating the Search input

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 const Search = () => {
   return (
-    <div>
-      
+    <div className='search'>
+      <div className="searchForm">
+        <input type="text" />
+      </div>
+      <div className="userChat">
+        <img src="https://cdn.pixabay.com/photo/2023/08/02/02/46/woman-8164186_1280.jpg" alt="" />
+        <div className="userChatInfo">
+          <span>Jane</span>
+        </div>
+      </div>
     </div>
   )
 }
