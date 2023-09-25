fde6f95ae7b9c44d3197ec5c4e536ec126db4c68
Patrick-Ngige <wakemanjajr@gmail.com>
2023-09-25 23:57:50 +0300

    creating the messages component

--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import Message from '../components/Message'
 
 const Messages = () => {
   return (
     <div className='messages'>
-      
+      <Message/>
+      <Message/>
+      <Message/>
+      <Message/>
+      <Message/>
+      <Message/>
     </div>
   )
 }
