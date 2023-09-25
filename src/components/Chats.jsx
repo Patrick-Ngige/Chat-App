452259c0cc1173d3d11d7c281ba33c1a3be78790
Patrick-Ngige <wakemanjajr@gmail.com>
2023-09-25 23:44:52 +0300

    duplicating the chats

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -10,6 +10,34 @@ const Chats = () => {
           <p>Hello there</p>
         </div>
       </div>
+      <div className="userChat">
+        <img src="https://cdn.pixabay.com/photo/2023/08/02/02/46/woman-8164186_1280.jpg" alt="" />
+        <div className="userChatInfo">
+          <span>Jane</span>
+          <p>Hello there</p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img src="https://cdn.pixabay.com/photo/2023/08/02/02/46/woman-8164186_1280.jpg" alt="" />
+        <div className="userChatInfo">
+          <span>Jane</span>
+          <p>Hello there</p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img src="https://cdn.pixabay.com/photo/2023/08/02/02/46/woman-8164186_1280.jpg" alt="" />
+        <div className="userChatInfo">
+          <span>Jane</span>
+          <p>Hello there</p>
+        </div>
+      </div>
+      <div className="userChat">
+        <img src="https://cdn.pixabay.com/photo/2023/08/02/02/46/woman-8164186_1280.jpg" alt="" />
+        <div className="userChatInfo">
+          <span>Jane</span>
+          <p>Hello there</p>
+        </div>
+      </div>
     </div>
   )
 }
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,7 +4,7 @@ const Search = () => {
   return (
     <div className='search'>
       <div className="searchForm">
-        <input type="text" placeholder='find a user'/>
+        <input type="text" placeholder='Find a user'/>
       </div>
       <div className="userChat">
         <img src="https://cdn.pixabay.com/photo/2023/08/02/02/46/woman-8164186_1280.jpg" alt="" />
