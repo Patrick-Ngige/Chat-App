32ab251192b2b50559cf6c3db23686407bd3046d
Patrick-Ngige <wakemanjajr@gmail.com>
2023-09-25 22:58:40 +0300

    styling the button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <div className='navbar'>
       <span className='logo'>Chat App</span>
       <div className="user">
-        <img src="" alt="" />
+        <img src="https://cdn.pixabay.com/photo/2023/08/02/02/46/woman-8164186_1280.jpg" alt="profile pic" />
         <span>John</span>
         <button>logout</button>
       </div>
