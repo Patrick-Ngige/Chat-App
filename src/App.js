098a174f9bb6023d530c3258c99b9ad6fcd5e1a7
Patrick-Ngige <wakemanjajr@gmail.com>
2023-09-26 18:19:42 +0300

    handleSubmit function

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import Home from './pages/Home';
 function App() {
   return (
     <>
-    <Home/>
-    {/* <Register/> */}
+    {/* <Home/> */}
+    <Register/>
     {/* <Login /> */}
     </>
   );
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import '../styles.scss'; 
 import Add from '../img/addAvatar.png';
 
-const Register = () => {
+const Register = (e) => {
+  e.preventDefault()
+  const handleSubmit = () => {
+
+  }
   return (
     <div className='formContainer'>
       <div className="formWrapper">
         <span className='logo'>Chat App</span>
         <span className='title'>Register</span>
-        <form>
+        <form onSubmit={handleSubmit}>
             <input type='text' placeholder='display name'/>
             <input type='email' placeholder='email'/>
             <input type='password' placeholder='password'/>
