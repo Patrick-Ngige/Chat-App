5d1c95102823f1639dd60b86af036a24a18e18de
Patrick-Ngige <wakemanjajr@gmail.com>
2023-09-26 12:57:02 +0300

    developing the message component

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -3,7 +3,14 @@ import React from 'react';
 const Message = () => {
   return (
     <div className='message'>
-      Message
+      <div className="messageinfo">
+      <img src="" alt="" />
+      <span>just now</span>
+      </div>
+      <div className="messageContent">
+        <p>hello</p>
+        <img src="" alt="" />
+      </div>
     </div>
   )
 }
