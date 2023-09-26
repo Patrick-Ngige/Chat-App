4c958b347a24268104e9598aa4d15f3336eea3c2
Patrick-Ngige <wakemanjajr@gmail.com>
2023-09-26 13:21:05 +0300

    styling the messageContent

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 const Message = () => {
   return (
-    <div className='message'>
+    <div className='message owner'>
       <div className="messageInfo">
       <img src="https://cdn.pixabay.com/photo/2023/08/02/02/46/woman-8164186_1280.jpg" alt="" />
       <span>just now</span>
       </div>
       <div className="messageContent">
         <p>hello</p>
-        {/* <img src="https://cdn.pixabay.com/photo/2023/08/02/02/46/woman-8164186_1280.jpg" alt="" /> */}
+        <img src="https://cdn.pixabay.com/photo/2023/08/02/02/46/woman-8164186_1280.jpg" alt="" />
       </div>
     </div>
   )
