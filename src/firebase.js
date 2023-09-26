0a52e39ec2dce48898feefc7e0db37f59acc8776
Patrick-Ngige <wakemanjajr@gmail.com>
2023-09-26 18:14:45 +0300

    installing and configuring firebase

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import getAuth from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: "chat-8a504.firebaseapp.com",
+  projectId: "chat-8a504",
+  storageBucket: "chat-8a504.appspot.com",
+  messagingSenderId: "714196355180",
+  appId: "1:714196355180:web:63e243388643b04ac93049"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth()
