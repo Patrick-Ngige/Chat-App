683c17c099a36773dea241253f7caa9bfb602338
Patrick-Ngige <wakemanjajr@gmail.com>
2023-09-26 12:13:34 +0300

    developing the chat input

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -3,7 +3,11 @@ import React from 'react'
 const Input = () => {
   return (
     <div className='input'>
-      Input
+      <input type="text" placeholder='Type something...' />
+      <div className="send">
+        <img src="" alt="" />
+        <input type="file" style={{display:"none"}} id="file" />
+      </div>
     </div>
   )
 }
