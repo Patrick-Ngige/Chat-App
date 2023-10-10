f1ca6a36a04180a131bd502e93a42299b2b157c9
Patrick-Ngige <wakemanjajr@gmail.com>
2023-10-10 15:49:01 +0300

    naming correction

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,14 +1,10 @@
-import React, { useState } from 'react';
-import '../styles.scss'; 
-import Add from '../img/addAvatar.png';
-import { createUserWithEmailAndPaasword, updateProfile} from 'firebase/auth';
-import {auth, storage, db} from '../firebase'
-import {
-  ref,
-  uploadBytesResumable,
-  getDownloadURL,
-} from "firebase/storage";
-import {doc, setDoc} from "firebase/firestore";
+import React, { useState } from "react";
+import "../styles.scss";
+import Add from "../img/addAvatar.png";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth, storage, db } from "../firebase";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { doc, setDoc } from "firebase/firestore";
 
 const Register = () => {
   const [err, setErr] = useState(false);
@@ -21,58 +17,55 @@ const Register = () => {
     const file = e.target[3].files[0];
 
     try {
-    const res = await createUserWithEmailAndPaasword(auth, email, password);
+      const res = await createUserWithEmailAndPassword(auth, email, password);
       const storageRef = ref(storage, displayName);
 
       const uploadTask = uploadBytesResumable(storageRef, file);
 
-    uploadTask.on(
-      (error) => {
-        setErr(true);
-      },
-      () => {
-        getDownloadURL(uploadTask.snapshot.ref).then( async(downloadURL) => {
-          await updateProfile(res.user,{
-            displayName,
-            photoURL:downloadURL,
-          })
-          await setDoc(doc(db, "users", res.user.uid), {
-            uid: res.user.uid,
-            displayName,
-            email,
-            photoURL:downloadURL
+      uploadTask.on(
+        (error) => {
+          setErr(true);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+            await updateProfile(res.user, {
+              displayName,
+              photoURL: downloadURL,
+            });
+            await setDoc(doc(db, "users", res.user.uid), {
+              uid: res.user.uid,
+              displayName,
+              email,
+              photoURL: downloadURL,
+            });
           });
-        });
-      }
-    );
-  
-
-  }catch (err) {
+        }
+      );
+    } catch (err) {
       setErr(true);
     }
-
   };
   return (
-    <div className='formContainer'>
+    <div className="formContainer">
       <div className="formWrapper">
-        <span className='logo'>Chat App</span>
-        <span className='title'>Register</span>
+        <span className="logo">Chat App</span>
+        <span className="title">Register</span>
         <form onSubmit={handleSubmit}>
-            <input type='text' placeholder='display name'/>
-            <input type='email' placeholder='email'/>
-            <input type='password' placeholder='password'/>
-            <input style={{display:"none"}} type="file" id='file' />
-            <label htmlFor="file">
-              <img src={Add} alt="Add an avatar" />
-              <span>Add an avatar</span>
-            </label>
-            <button>Sign Up</button>
-            {err && <span>Something went wrong.s</span>}
+          <input type="text" placeholder="display name" />
+          <input type="email" placeholder="email" />
+          <input type="password" placeholder="password" />
+          <input style={{ display: "none" }} type="file" id="file" />
+          <label htmlFor="file">
+            <img src={Add} alt="Add an avatar" />
+            <span>Add an avatar</span>
+          </label>
+          <button>Sign Up</button>
+          {err && <span>Something went wrong.s</span>}
         </form>
         <p>You do have an account? Login</p>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
