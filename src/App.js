bdbbbf4d35ebf43e5d5fa76492d6a50ccb691a4f
Patrick-Ngige <wakemanjajr@gmail.com>
2023-10-11 23:02:21 +0300

    wrapping react.stringmode within authContextProvider tags in index.js

--- src/App.js
+++ src/App.js
@@ -7,8 +7,13 @@ import {
   Routes,
   Route,
 } from "react-router-dom"
+import { useContext } from 'react';
+import { AuthContext } from './context/AuthContext';
 
 function App() {
+
+  const {currentUser} = useContext(AuthContext)
+
   return (
     <BrowserRouter>
     <Routes>
