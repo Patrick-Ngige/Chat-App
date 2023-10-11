6e34ea09c9a78618ad994138a86b7471536dfa22
Patrick-Ngige <wakemanjajr@gmail.com>
2023-10-11 22:17:20 +0300

    integrating react router dom in app.jjs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import Register from './pages/Register';
 import Login from './pages/Login.jsx'
 import Home from './pages/Home';
+import "./styles.scss"
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom"
 
 function App() {
   return (
