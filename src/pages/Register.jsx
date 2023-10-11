0dc3f17521ffdf4b082b752080e660ae969b1cc0
Patrick-Ngige <wakemanjajr@gmail.com>
2023-10-11 22:30:50 +0300

    using useNavigate in register.jsx

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -5,9 +5,11 @@ import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import { auth, storage, db } from "../firebase";
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import { doc, setDoc } from "firebase/firestore";
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
   const [err, setErr] = useState(false);
+  const navigate = useNavigate()
 
   const handleSubmit = async (e) => {
     e.preventDefault();
