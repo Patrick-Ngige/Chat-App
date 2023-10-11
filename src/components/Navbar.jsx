34d30901c46146282262e707e28242691a6b357c
Patrick-Ngige <wakemanjajr@gmail.com>
2023-10-11 23:08:04 +0300

    sign out button functionality

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { signOut } from 'firebase/auth'
 import React from 'react'
+import { auth } from '../firebase'
 
 const Navbar = () => {
   return (
@@ -8,7 +9,7 @@ const Navbar = () => {
       <div className="user">
         <img src="https://cdn.pixabay.com/photo/2023/08/02/02/46/woman-8164186_1280.jpg" alt="profile pic" />
         <span>John</span>
-        <button onClick={()=>signOut()}>logout</button>
+        <button onClick={()=>signOut(auth)}>logout</button>
       </div>
     </div>
   )
