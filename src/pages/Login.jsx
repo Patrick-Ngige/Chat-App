60cd4d086ef1abfd61bc8a16ab32613967c0c22d
Patrick-Ngige <wakemanjajr@gmail.com>
2023-10-13 14:59:53 +0300

    using the try catch block in login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,12 +2,29 @@ import React from 'react';
 import '../styles.scss'; 
 
 const Login = () => {
+
+  const [err, setErr] = useState(false);
+  const navigate = useNavigate()
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const email = e.target[0].value;
+    const password = e.target[1].value;
+   
+
+    try {
+      
+    } catch (err) {
+      setErr(true);
+    }
+  };
+
   return (
     <div className='formContainer'>
       <div className="formWrapper">
         <span className='logo'>Chat App</span>
         <span className='title'>Login</span>
-        <form>
+        <form onSubmit={handleSubmit}>
             <input type='email' placeholder='email'/>
             <input type='password' placeholder='password'/>
             <button>Sign In</button>
