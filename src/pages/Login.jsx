b7f7d125343c8cc1183f8d2d512e6d3215e948cd
Patrick-Ngige <wakemanjajr@gmail.com>
2023-10-16 16:49:15 +0300

    error message setup

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -32,7 +32,7 @@ const Login = () => {
             <input type='email' placeholder='email'/>
             <input type='password' placeholder='password'/>
             <button>Sign In</button>
-            {err && <span>Something went wrong.s</span>}
+            {err && <span>Something went wrong</span>}
         </form>
         <p>You don't have an account? <Link to="/register">Register</Link> </p>
       </div>
