e9a2be5530847438015d7cad9c243eb992063d9f
Patrick-Ngige <wakemanjajr@gmail.com>
2023-10-17 14:30:01 +0300

    solving api issue

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,14 @@ import {getAuth} from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import {getStorage} from "firebase/storage";
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: "chat-8a504.firebaseapp.com",
-  projectId: "chat-8a504",
-  storageBucket: "chat-8a504.appspot.com",
-  messagingSenderId: "714196355180",
-  appId: "1:714196355180:web:63e243388643b04ac93049"
+  apiKey: process.env.REACT_FIREBASE_API_KEY,
+  authDomain: "chat-app-73ad1.firebaseapp.com",
+  projectId: "chat-app-73ad1",
+  storageBucket: "chat-app-73ad1.appspot.com",
+  messagingSenderId: "512733840355",
+  appId: "1:512733840355:web:85aa11693a241d09d956d6"
 };
 
 // Initialize Firebase
