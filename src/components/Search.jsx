3bd4cb178d0d2cdb95cbf038c3d69a28371e4242
Patrick-Ngige <wakemanjajr@gmail.com>
2023-10-17 17:42:28 +0300

    setting up the search method

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,17 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { db } from '../firebase';
+import { collection, getDocs, query, where } from 'firebase/firestore';
 
 const Search = () => {
+
+  const [username, setUsername] = useState("");
+  const [user, setUser] = useState(null);
+  const [err, setErr] = useState(false);
+
+  const handleSearch = async () => {
+    const q = query(
+      collection(db, "users"),
+      where("displayName", "==", username)
+    )
+    
+    try{
+    const querySnapshot = await getDocs(q);
+    querySnapshot.forEach((doc) => {
+        setUser(doc.data())
+      })
+    }catch (err){
+      setErr(true)
+    }
+  }
+
+  const handleKey = e => {
+    e.code === "Enter" && handleSearch();
+  }
+
   return (
     <div className='search'>
       <div className="searchForm">
-        <input type="text" placeholder='Find a user'/>
+        <input type="text" placeholder='Find a user' onKeyDown={handleKey} onChange={e=>setUsername(e.target.value)}/>
       </div>
-      <div className="userChat">
-        <img src="https://cdn.pixabay.com/photo/2023/08/02/02/46/woman-8164186_1280.jpg" alt="" />
+      {err && <span>User not found</span>}
+      {user && <div className="userChat">
+        <img src={user.photoURL} alt='' />
         <div className="userChatInfo">
-          <span>Jane</span>
+          <span>{user.displayName}</span>
         </div>
-      </div>
+      </div>}
     </div>
   )
 }
